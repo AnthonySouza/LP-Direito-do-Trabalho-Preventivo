8a944a6c7345dc3915a019ed3af1dd59788b0270
AnthonySouza <gabrieldaniel1798@gmail.com>
2022-08-22 17:23:01 -0300

    Agora Responsivo

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,8 @@
 const navbar = document.querySelector("#lp_navbar");
 const header = document.querySelector("#lp_init__section");
+const top_button = document.querySelector(".btn_top");
+
+top_button.style.visibility = "hidden";
 
 //const forest = document.querySelector(".forest");
 const silhouette = document.querySelector(".lp_face__container");
@@ -10,9 +13,15 @@ window.onscroll = () => {
 
   
 
-  if (scrollPos - 100 <= window.innerHeight)
-    header.style.visibility = header.style.visibility === "hidden" && "visible";
-  else header.style.visibility = "hidden";
+  if (scrollPos - 100 <= window.innerHeight) {
+
+    top_button.style.visibility = "hidden";
+
+  } else {
+
+    top_button.style.visibility = "visible";
+
+  }
 
   if (scrollPos + 100 >= window.innerHeight) navbar.classList.add("nav__active");
   else navbar.classList.remove("nav__active");
